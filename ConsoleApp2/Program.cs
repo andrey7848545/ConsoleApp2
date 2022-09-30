93f1d9f4ec3bf7e0d17016442b67afa73ae58a32
andre <andre@DESKTOP-9RC7R2U>
2022-09-30 20:10:27 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Люков Андрей 22-ИСП-2/2 
+try 
+{
+    Console.WriteLine("введите длину основания а трапеции");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine("введите длину основания b трапеции");
+    double b = double.Parse(Console.ReadLine());
+    Console.WriteLine("введите высоту h трапеции");
+    double h = double.Parse(Console.ReadLine());
+    double res = Math.Sqrt(Math.Pow(Math.Abs(a - b) / 2, 2) + Math.Pow(h, 2)) * 2 + a + b;
+    Console.WriteLine("периметр трапеции");
+    Console.WriteLine(res);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString);
+}
